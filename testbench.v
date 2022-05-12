33eb347d79d3548c61afdfd6ef364becdce745dc
movingpictures83 <movingpictures83@gmail.com>
2022-05-12 07:56:15 -0400

    Added color New format for testbench

--- testbench.v
+++ testbench.v
@@ -1,142 +1,63 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Testbench for Component: MUX32
+// Package: FIUSCIS-CDA
+// Course: CDA3102 (Computer Architecture), Florida International University
+// Developer: Trevor Cickovski
+// License: MIT, (C) 2020 All Rights Reserved
+///////////////////////////////////////////////////////////////////////////////////
+
+
 module testbench();
-
-reg I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15, I16, I17, I18, I19, I20, I21, I22, I23, I24, I25, I26, I27, I28, I29, I30, I31;
+`include "../Test/Test.v"
+///////////////////////////////////////////////////////////////////////////////////
+// Inputs: I0-I31 (1-bit each);  S (4-bit)
+reg[31:0] I;
 reg[4:0] S;
-wire Y;
+///////////////////////////////////////////////////////////////////////////////////
 
-MUX32 myMUX(I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15, I16, I17, I18, I19, I20, I21, I22, I23, I24, I25, I26, I27, I28, I29, I30, I31, S, Y);
+///////////////////////////////////////////////////////////////////////////////////
+// Output: Y (1-bit)
+wire Y;
+///////////////////////////////////////////////////////////////////////////////////
+
+MUX32 myMUX(.I0(I[0]),.I1(I[1]),.I2(I[2]),.I3(I[3]),.I4(I[4]),.I5(I[5]),.I6(I[6]),.I7(I[7]),
+.I8(I[8]),.I9(I[9]),.I10(I[10]),.I11(I[11]),.I12(I[12]),.I13(I[13]),.I14(I[14]),.I15(I[15]),
+.I16(I[16]),.I17(I[17]),.I18(I[18]),.I19(I[19]),.I20(I[20]),.I21(I[21]),.I22(I[22]),.I23(I[23]),
+.I24(I[24]),.I25(I[25]),.I26(I[26]),.I27(I[27]),.I28(I[28]),.I29(I[29]),.I30(I[30]),.I31(I[31]),.S(S),.Y(Y));
+
+integer j;
 
 initial begin
-I0=1;I1=0;I2=0;I3=0;I4=0;I5=0;I6=0;I7=0;I8=0;I9=0;I10=0;I11=0;I12=0;I13=0;I14=0;I15=0;I16=0;I17=0;I18=0;I19=0;I20=0;I21=0;I22=0;I23=0;I24=0;I25=0;I26=0;I27=0;I28=0;I29=0;I30=0;I31=0;S=5'b00000; #10; 
-if (Y !== I0) begin
-           $display("Error: Expected %d got %d for S=%d", I0, Y, S); $stop;
-end
-I0=0;I1=1;S=5'b00001; #10; 
-if (Y !== I1) begin
-           $display("Error: Expected %d got %d for S=%d", I1, Y, S); $stop;
-end
-I1=0;I2=1;S=5'b00010; #10; 
-if (Y !== I2) begin
-           $display("Error: Expected %d got %d for S=%d", I2, Y, S); $stop;
-end
-I2=0;I3=1;S=5'b00011; #10; 
-if (Y !== I3) begin
-           $display("Error: Expected %d got %d for S=%d", I3, Y, S); $stop;
-end
-I3=0;I4=1;S=5'b00100; #10; 
-if (Y !== I4) begin
-           $display("Error: Expected %d got %d for S=%d", I4, Y, S); $stop;
-end
-I4=0;I5=1;S=5'b00101;  #10; 
-if (Y !== I5) begin
-           $display("Error: Expected %d got %d for S=%d", I5, Y, S); $stop;
-end
-I5=0;I6=1;S=5'b00110;  #10; 
-if (Y !== I6) begin
-           $display("Error: Expected %d got %d for S=%d", I6, Y, S); $stop;
-end
-I6=0;I7=1;S=5'b00111;  #10; 
-if (Y !== I7) begin
-           $display("Error: Expected %d got %d for S=%d", I7, Y, S); $stop;
-end
-I7=0;I8=1;S=5'b01000;  #10; 
-if (Y !== I8) begin
-           $display("Error: Expected %d got %d for S=%d", I8, Y, S); $stop;
-end
-I8=0;I9=1;S=5'b01001;  #10; 
-if (Y !== I9) begin
-           $display("Error: Expected %d got %d for S=%d", I9, Y, S); $stop;
-end
-I9=0;I10=1; S=5'b01010; #10; 
-if (Y !== I10) begin
-           $display("Error: Expected %d got %d for S=%d", I10, Y, S); $stop;
-end
-I10=0;I11=1;S=5'b01011; #10; 
-if (Y !== I11) begin
-           $display("Error: Expected %d got %d for S=%d", I11, Y, S); $stop;
-end
-I11=0;I12=1;S=5'b01100; #10; 
-if (Y !== I12) begin
-           $display("Error: Expected %d got %d for S=%d", I12, Y, S); $stop;
-end
-I12=0;I13=1;S=5'b01101; #10; 
-if (Y !== I13) begin
-           $display("Error: Expected %d got %d for S=%d", I13, Y, S); $stop;
-end
-I13=0;I14=1;S=5'b01110; #10; 
-if (Y !== I14) begin
-           $display("Error: Expected %d got %d for S=%d", I14, Y, S); $stop;
-end
-I14=0;I15=1;S=5'b01111; #10; 
-if (Y !== I15) begin
-           $display("Error: Expected %d got %d for S=%d", I15, Y, S); $stop;
-end
-I15=0;I16=1;S=5'b10000; #10; 
-if (Y !== I16) begin
-           $display("Error: Expected %d got %d for S=%d", I16, Y, S); $stop;
-end
-I16=0;I17=1;S=5'b10001;  #10; 
-if (Y !== I17) begin
-           $display("Error: Expected %d got %d for S=%d", I17, Y, S); $stop;
-end
-I17=0;I18=1;S=5'b10010;  #10; 
-if (Y !== I18) begin
-           $display("Error: Expected %d got %d for S=%d", I18, Y, S); $stop;
-end
-I18=0;I19=1; S=5'b10011; #10; 
-if (Y !== I19) begin
-           $display("Error: Expected %d got %d for S=%d", I19, Y, S); $stop;
-end
-I19=0;I20=1;S=5'b10100;  #10; 
-if (Y !== I20) begin
-           $display("Error: Expected %d got %d for S=%d", I20, Y, S); $stop;
-end
-I20=0;I21=1;S=5'b10101; #10; 
-if (Y !== I21) begin
-           $display("Error: Expected %d got %d for S=%d", I21, Y, S); $stop;
-end
-I21=0;I22=1;S=5'b10110; #10; 
-if (Y !== I22) begin
-           $display("Error: Expected %d got %d for S=%d", I22, Y, S); $stop;
-end
-I22=0;I23=1;S=5'b10111; #10; 
-if (Y !== I23) begin
-           $display("Error: Expected %d got %d for S=%d", I23, Y, S); $stop;
-end
-I23=0;I24=1;S=5'b11000; #10; 
-if (Y !== I24) begin
-           $display("Error: Expected %d got %d for S=%d", I24, Y, S); $stop;
-end
-I24=0;I25=1;S=5'b11001; #10; 
-if (Y !== I25) begin
-           $display("Error: Expected %d got %d for S=%d", I25, Y, S); $stop;
-end
-I25=0;I26=1;S=5'b11010; #10; 
-if (Y !== I26) begin
-           $display("Error: Expected %d got %d for S=%d", I26, Y, S); $stop;
-end
-I26=0;I27=1;S=5'b11011; #10; 
-if (Y !== I27) begin
-           $display("Error: Expected %d got %d for S=%d", I27, Y, S); $stop;
-end
-I27=0;I28=1;S=5'b11100; #10; 
-if (Y !== I28) begin
-           $display("Error: Expected %d got %d for S=%d", I28, Y, S); $stop;
-end
-I28=0;I29=1;S=5'b11101;  #10; 
-if (Y !== I29) begin
-           $display("Error: Expected %d got %d for S=%d", I29, Y, S); $stop;
-end
-I29=0;I30=1;S=5'b11110;  #10; 
-if (Y !== I30) begin
-           $display("Error: Expected %d got %d for S=%d", I30, Y, S); $stop;
-end
-I30=0;I31=1;S=5'b11111;  #10; 
-if (Y !== I31) begin
-           $display("Error: Expected %d got %d for S=%d", I31, Y, S); $stop;
-end
 
+////////////////////////////////////////////////////////////////////////////////////////
+// I[0] to 1, rest to 0
+I[0]=1;
+for (j = 1; j <= 31; j = j + 1) begin
+  I[j] = 0;
+end
+////////////////////////////////////////////////////////////////////////////////////////
 
+////////////////////////////////////////////////////////////////////////////////////////
+// Test: S=00000
+$display("Testing: S=00000");
+S=5'b00000; #10;
+verifyEqual(Y, 1);
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+//  Testing: All 5-bit S values 1 to 31
+for (S=5'b00001; S <= 5'b11111; S = S + 5'b00001) begin
+   I[S-1] = 0; // previous to zero
+   I[S] = 1; // current to one
+   $display("Testing: S=%b", S);
+   #10;
+   verifyEqual(Y, 1);
+   // You need this because the counter will reset to 0 otherwise
+   if (S == 5'b11111) begin
+    $display("All tests passed.");
+    $stop;
+   end
+end
+////////////////////////////////////////////////////////////////////////////////////////
 $display("All tests passed.");
 end
 
